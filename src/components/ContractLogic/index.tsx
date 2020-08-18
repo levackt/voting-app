d490bed243a69c666bed59e515bc731a0ca6fa65
Abel Fernández <abel.fernandez.nandin@gmail.com>
2020-08-18 16:14:29 +0200

    Update initMsg usage

--- src/components/ContractLogic/index.tsx
+++ src/components/ContractLogic/index.tsx
@@ -13,53 +13,51 @@ const emptyInfo: State = {
   owner: "",
   stakedTokens: 0,
   pollCount: 0,
-  initMsg: {denom: "", name: ""},
+  initMsg: { denom: "", name: "" },
 };
 
-type State = { 
-  readonly address: string,
-  readonly owner?: string,
-  readonly stakedTokens?: number,
-  readonly pollCount?: number,
-  readonly initMsg: InitMsg,
+type State = {
+  readonly address: string;
+  readonly owner?: string;
+  readonly stakedTokens?: number;
+  readonly pollCount?: number;
+  readonly initMsg?: InitMsg;
 };
 
 function ContractLogic({ address }: ContractDetailsProps): JSX.Element {
   const { getClient } = useSdk();
   const { setError } = useError();
-  
 
-  const [value, setValue,] = React.useState<State>(emptyInfo);
+  const [value, setValue] = React.useState<State>(emptyInfo);
 
   // get the contracts
   React.useEffect(() => {
     getClient()
       .getContract(address)
       .then(info => {
-          setValue({ ...info, address})
-          getClient()
+        setValue({ ...info, address });
+        getClient()
           /* eslint-disable-next-line @typescript-eslint/camelcase */
-          .queryContractSmart(address, { config: { } })
+          .queryContractSmart(address, { config: {} })
           .then(res => {
-            setValue({ 
+            setValue({
               address: address,
               stakedTokens: res.staked_tokens,
               pollCount: res.poll_count,
-              initMsg: info.initMsg,
-              })
+              initMsg: { denom: res.denom, name: info.label },
+            });
           });
-        }
-      )
+      })
       .catch(setError);
   }, [setError, address, getClient]);
 
   return (
     <div>
-      {value.initMsg && value.initMsg.denom &&
+      {value.initMsg && value.initMsg.denom && (
         <div>
           <VotingDetails contractAddress={address} owner={value.owner || ""} contract={value.initMsg} />
         </div>
-      }
+      )}
     </div>
   );
 }
