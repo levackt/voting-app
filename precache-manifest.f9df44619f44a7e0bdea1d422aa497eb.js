8a4d660a8b0780933813d52e44f61d428a03683e
Taariq Levack <levackt@users.noreply.github.com>
2020-08-26 06:09:11 +0200

    Updates

---
+++ precache-manifest.f9df44619f44a7e0bdea1d422aa497eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fdbb481737853c08ff59d7bffc17d8",
+    "url": "/voting-app/index.html"
+  },
+  {
+    "revision": "0e935f56bb9709a038db",
+    "url": "/voting-app/static/css/main.f319df4c.chunk.css"
+  },
+  {
+    "revision": "bfe550b0f74bd6120984",
+    "url": "/voting-app/static/js/2.c6eb0893.chunk.js"
+  },
+  {
+    "revision": "b93ece77fcbdc2e55817f33239e857b8",
+    "url": "/voting-app/static/js/2.c6eb0893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e935f56bb9709a038db",
+    "url": "/voting-app/static/js/main.5ee3fa1e.chunk.js"
+  },
+  {
+    "revision": "4fc2fe940ecfc033ab4e",
+    "url": "/voting-app/static/js/runtime-main.b9137f1f.js"
+  }
+]);
